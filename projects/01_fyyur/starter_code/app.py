806b824663bb222b52bf76ada9bbc841d540de71
Baraa <baraamashaal@gmail.com>
2020-07-18 22:02:03 +0200

    Fyyur: insert form data as a new Venue record in the db, instead

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -13,6 +13,7 @@ from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
 from flask_migrate import Migrate
+import sys
 
 # ----------------------------------------------------------------------------#
 # App Config.
@@ -250,13 +251,39 @@ def create_venue_form():
 
 @app.route('/venues/create', methods=['POST'])
 def create_venue_submission():
-    # TODO: insert form data as a new Venue record in the db, instead
-    # TODO: modify data to be the data object returned from db insertion
+    error = False
+    try:
+        # DONE: insert form data as a new Venue record in the db, instead
+        name = request.form['name']
+        city = request.form['city']
+        state = request.form['state']
+        address = request.form['address']
+        phone = request.form['phone']
+        image_link = request.form['image_link']
+        facebook_link = request.form['facebook_link']
+        website = request.form['website']
+        genres = request.form.getlist('genres')
+        seeking_talent = True if 'seeking_talent' in request.form else False
+        seeking_description = request.form['seeking_description']
+        # DONE: modify data to be the data object returned from db insertion
+        venue = Venue(name=name, city=city, state=state, address=address, phone=phone, image_link=image_link,
+                      facebook_link=facebook_link, website=website, genres=genres, seeking_talent=seeking_talent,
+                      seeking_description=seeking_description)
+        db.session.add(venue)
+        db.session.commit()
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+        if error:
+            # DONE: on unsuccessful db insert, flash an error instead.
+            flash('An error occurred. Venue ' + request.form['name'] + ' could not be listed.')
+        else:
+            # on successful db insert, flash success
+            flash('Venue ' + request.form['name'] + ' was successfully listed!')
 
-    # on successful db insert, flash success
-    flash('Venue ' + request.form['name'] + ' was successfully listed!')
-    # TODO: on unsuccessful db insert, flash an error instead.
-    # e.g., flash('An error occurred. Venue ' + data.name + ' could not be listed.')
     # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
     return render_template('pages/home.html')
 
