5b70521207c0b0c6f4b3fd194cc9688d39bb4538
Baraa <baraamashaal@gmail.com>
2020-07-20 08:38:11 +0200

    Fyyur: insert form data as a new Show record in the db, instead, on unsuccessful db insert, flash an error instead.

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -522,13 +522,30 @@ def create_shows():
 @app.route('/shows/create', methods=['POST'])
 def create_show_submission():
     # called to create new shows in the db, upon submitting new show listing form
-    # TODO: insert form data as a new Show record in the db, instead
+    # DONE: insert form data as a new Show record in the db, instead
+    error = False
+    try:
+        artist_id = int(request.form['artist_id'])
+        venue_id = int(request.form['venue_id'])
+        start_time = request.form['start_time']
+        show = Show(artist_id=artist_id,venue_id=venue_id,start_time=start_time)
+        db.session.add(show)
+        db.session.commit()
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+        if error:
+            # DONE: on unsuccessful db insert, flash an error instead.
+            # e.g., flash('An error occurred. Show could not be listed.')
+            # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+            flash('An error occurred. Show could not be listed.')
+        else:
+            # on successful db insert, flash success
+            flash('Show was successfully listed!')
 
-    # on successful db insert, flash success
-    flash('Show was successfully listed!')
-    # TODO: on unsuccessful db insert, flash an error instead.
-    # e.g., flash('An error occurred. Show could not be listed.')
-    # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
     return render_template('pages/home.html')
 
 
